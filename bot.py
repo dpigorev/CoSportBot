4f611099733dbfdaa3dec50af3e12b4a12e0482b
VolokitinA <87081494+VolokitinA@users.noreply.github.com>
2021-07-12 20:26:32 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -20,7 +20,9 @@ class Step(StatesGroup):
     reg_email = State()
     reg_isSportsman = State()
     reg_gender = State()
-    #reg_notValid_email = State()
+
+conn = psycopg2.connect(host="localhost", port = 5432, database="CoSport", user="postgres", password="1723") #база данных
+cur = conn.cursor()
 
 bot_token = getenv("BOT_TOKEN")
 if not bot_token:
@@ -33,8 +35,6 @@ logging.basicConfig(level=logging.INFO)
 
 
 registr_arr = []
-phone_number = ""
-hasBeenRepeated = False
 
 async def check(message):
     url = "http://167.172.35.241/CSDB/Users/?Content-Type=application/json&Nickname=qartz"
@@ -45,22 +45,21 @@ async def check(message):
     print(response1 + "пусто")
 dp.register_message_handler(check, commands="check")
 
+async def show(message):
+    await message.answer(message.from_user.id)
+dp.register_message_handler(show, commands="show")
 
 async def add(message):
     requests.post('http://167.172.35.241/CSDB/Users/?Content-Type=application/json',
-                  data={'Phone': '79209217373', 'Nickname': 'aboba', 'Password': 'qwerty',
+                  data={'Phone': '79209217373', 'Nickname': 'adfsf', 'Password': 'qwerty',
                         'Name': 'Ivan', 'Surname': 'Ivanov', 'Email': 'artjom200006@yandex.ru',
                         'Gender': 'M', 'Profsportman': 'True'})
 dp.register_message_handler(add, commands="add")
 
 
 async def cmd_phonenumber(message):
-    global phone_number
     global registr_arr
-    global hasBeenRepeated
-    phone_number = ""
     registr_arr = []
-    hasBeenRepeated = False
     keyboard = types.ReplyKeyboardMarkup()
     button_1 = types.KeyboardButton(text="Авторизоваться по номеру телефона", request_contact=True)
     keyboard.add(button_1)
@@ -68,8 +67,8 @@ async def cmd_phonenumber(message):
 dp.register_message_handler(cmd_phonenumber, commands="start")
 
 async def autorization(message):
-    global phone_number
     phone_number = str(message.contact.phone_number)
+    #print(phone_number)
     zapros = 'http://167.172.35.241/CSDB/Users/?Content-Type=application/json&Phone=' + phone_number
     response = requests.get(zapros)
     response1 = response.text
@@ -93,19 +92,25 @@ async def autorization(message):
         button_6 = types.KeyboardButton(text="Настройки")
         keyboard.add(button_6)
         await message.answer("Здравствуйте, " + Name + "!", reply_markup=keyboard)
-
+        await message.answer("id: " + str(message.from_user.id))
     else:
         keyboard = types.ReplyKeyboardMarkup()
         button_1 = types.KeyboardButton(text="Зарегистрироваться!")
         keyboard.add(button_1)
+        registr_arr.append(phone_number)
+
+
+        zap = "insert into Users (phone, userid) values ('" + phone_number + "'" + ", '" + str(message.from_user.id) + "')"
+        cur.execute(zap)
+        conn.commit()
+
         await Step.reg_start.set()
-        await message.answer("Вы не зарагистрированны на нашем сервисе. Нажмите на кнопку 'Зарегистрироваться!' чтобы пройти регистрацию. ", reply_markup=keyboard)
+        await message.answer("Вы не зарегистрированны на нашем сервисе. Нажмите на кнопку 'Зарегистрироваться!' чтобы пройти регистрацию. ", reply_markup=keyboard)
 dp.register_message_handler(autorization, content_types=["contact"])
 
 async def registration(message, state):
     keyboard = types.ReplyKeyboardRemove()
     #global phone_number
-    registr_arr.append(phone_number)
     await state.finish()
     await Step.reg_nickname.set()
     await message.answer("Введите Nickname", reply_markup=keyboard)
@@ -149,6 +154,11 @@ async def registr_first_step(message: types.Message, state):
                 break
     if cont:
         registr_arr.append(nickname)
+
+        zap = "update Users set nickname = '" + nickname + "' where userid = '" + str(message.from_user.id) + "'"
+        cur.execute(zap)
+        conn.commit()
+
         await message.answer("Ваш Nickname - " + nickname)
         await message.answer("Введите пароль\nВнимание! Пароль должен содержать не менее 6 символов!")
         await state.finish()
@@ -157,7 +167,7 @@ dp.register_message_handler(registr_first_step, state=Step.reg_nickname)
 
 async def registr_second_step(message: types.Message, state):
     global registr_arr
-    global hasBeenRepeated
+    hasBeenRepeated = False
     pas = message.text
     #print(pas)
     repeat = False
@@ -167,6 +177,11 @@ async def registr_second_step(message: types.Message, state):
         repeat = True
         hasBeenRepeated = True
     if repeat == False:
+
+        zap = "update Users set password = '" + pas + "' where userid = '" + str(message.from_user.id) + "'"
+        cur.execute(zap)
+        conn.commit()
+
         registr_arr.append(message.text)
         await message.answer("Введите имя")
         await state.finish()
@@ -178,6 +193,11 @@ dp.register_message_handler(registr_second_step, state=Step.reg_pass)
 
 async def registr_third_step(message: types.Message, state):
     global registr_arr
+
+    zap = "update Users set name = '" + message.text + "' where userid = '" + str(message.from_user.id) + "'"
+    cur.execute(zap)
+    conn.commit()
+
     registr_arr.append(message.text)
     await message.answer("Введите фамилию")
     await state.finish()
@@ -187,6 +207,11 @@ dp.register_message_handler(registr_third_step, state=Step.reg_name)
 async def registr_fourth_step(message: types.Message, state):
     global registr_arr
     registr_arr.append(message.text)
+
+    zap = "update Users set surname = '" + message.text + "' where userid = '" + str(message.from_user.id) + "'"
+    cur.execute(zap)
+    conn.commit()
+
     await message.answer("Введите E-mail")
     await state.finish()
     await Step.reg_email.set()
@@ -212,6 +237,11 @@ async def registr_fifth_step(message: types.Message, state):
             await Step.reg_email.set()
         else:
             registr_arr.append(email)
+
+            zap = "update Users set email = '" + email + "' where userid = '" + str(message.from_user.id) + "'"
+            cur.execute(zap)
+            conn.commit()
+
             keyboard = types.ReplyKeyboardMarkup()
             button_1 = types.KeyboardButton(text="Мужской")
             keyboard.add(button_1)
@@ -227,9 +257,19 @@ async def registr_sixth_step(message: types.Message, state):
     cont = False
     if message.text == "Мужской":
         registr_arr.append("Мужчина")
+
+        zap = "update Users set gender = '" + "М" + "' where userid = '" + str(message.from_user.id) + "'"
+        cur.execute(zap)
+        conn.commit()
+
         cont = True
     elif message.text == "Женский":
         registr_arr.append("Женщина")
+
+        zap = "update Users set gender = '" + "Ж" + "' where userid = '" + str(message.from_user.id) + "'"
+        cur.execute(zap)
+        conn.commit()
+
         cont = True
     else:
         await state.finish()
@@ -256,9 +296,19 @@ async def registr_seventh_step(message: types.Message, state):
     cont = False
     if message.text == "Да":
         registr_arr.append(True)
+
+        zap = "update Users set sport = " + "True" + " where userid = '" + str(message.from_user.id) + "'"
+        cur.execute(zap)
+        conn.commit()
+
         cont = True
     elif message.text == "Нет":
         registr_arr.append(False)
+
+        zap = "update Users set sport = " + "False" + " where userid = '" + str(message.from_user.id) + "'"
+        cur.execute(zap)
+        conn.commit()
+
         cont = True
     else:
         await state.finish()
@@ -270,7 +320,6 @@ async def registr_seventh_step(message: types.Message, state):
         keyboard.add(button_2)
         await message.answer("Введены некорректные данные. Пожалуйста, используйте кнопки.", reply_markup=keyboard)
     if cont:
-        keyboard = types.ReplyKeyboardRemove()
         await state.finish()
         keyboard = types.ReplyKeyboardMarkup()
         button_1 = types.KeyboardButton(text="Создать команду")
